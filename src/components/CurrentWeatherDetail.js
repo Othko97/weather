f23b4e86ad3bc8475299b206dd5cbd5dec1bf063
Jack Coldrick <jcldrk@gmail.com>
2017-10-21 20:43:35 +0100

    Add title to CurrentWeatherDetail component.

--- src/components/CurrentWeatherDetail.js
+++ src/components/CurrentWeatherDetail.js
@@ -6,7 +6,7 @@ class CurrentWeatherDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            timeOfDay : '',
+            timeOfDay: '',
             sunrise: '',
             sunset: ''
         };
@@ -17,7 +17,7 @@ class CurrentWeatherDetail extends Component {
             let date = new Date();
             let timeOfDay = this.compareTimes(date, nextProps.weatherData.sys.sunrise, nextProps.weatherData.sys.sunset);
 
-            let sunriseObj = new Date(nextProps.weatherData.sys.sunrise*1000);
+            let sunriseObj = new Date(nextProps.weatherData.sys.sunrise * 1000);
 
             let sunriseMinutes = sunriseObj.getMinutes().toString();
             if (sunriseMinutes.length === 1) {
@@ -26,7 +26,7 @@ class CurrentWeatherDetail extends Component {
 
             let sunrise = `${sunriseObj.getHours()}:${sunriseMinutes}`;
 
-            let sunsetObj = new Date(nextProps.weatherData.sys.sunset*1000);
+            let sunsetObj = new Date(nextProps.weatherData.sys.sunset * 1000);
 
             let sunsetMinutes = sunsetObj.getMinutes().toString();
             if (sunsetMinutes.length === 1) {
@@ -35,7 +35,7 @@ class CurrentWeatherDetail extends Component {
 
             let sunset = `${sunsetObj.getHours()}:${sunsetMinutes}`;
 
-            this.setState({timeOfDay, sunrise, sunset});
+            this.setState({ timeOfDay, sunrise, sunset });
 
             return;
         }
@@ -43,7 +43,7 @@ class CurrentWeatherDetail extends Component {
 
     compareTimes(curTimeObject, sunrise, sunset) {
         let timestamp = curTimeObject.getTime();
-        timestamp = Math.floor(timestamp / 1e03);        
+        timestamp = Math.floor(timestamp / 1e03);
         if (timestamp < sunrise || timestamp > sunset) {
             let timeOfDay = 'night';
             return timeOfDay;
@@ -59,51 +59,56 @@ class CurrentWeatherDetail extends Component {
         }
 
         return (
-            <div className="current-weather-detail">
+            <div>
+                <div className="title-container">
+                    <h1>Current Weather</h1>
+                </div>
+                <div className="current-weather-detail">
 
-             <div className="weather-card">
-                 <h3>Weather</h3>
-                 <h3>{this.props.weatherData.weather[0].description}</h3>
-                 <i className={`wi wi-owm-${this.state.timeOfDay}-${this.props.weatherData.weather[0].id}`}></i>
-             </div>
+                    <div className="weather-card">
+                        <h3>Weather</h3>
+                        <h3>{this.props.weatherData.weather[0].description}</h3>
+                        <i className={`wi wi-owm-${this.state.timeOfDay}-${this.props.weatherData.weather[0].id}`}></i>
+                    </div>
 
-             <div className="weather-card">
-                 <h3>Temperature</h3>
-                 <h3>{this.props.weatherData.main.temp}&#8451;</h3>
-                 <i className="wi wi-thermometer"></i>
-             </div>
+                    <div className="weather-card">
+                        <h3>Temperature</h3>
+                        <h3>{this.props.weatherData.main.temp}&#8451;</h3>
+                        <i className="wi wi-thermometer"></i>
+                    </div>
 
-             <div className="weather-card">
-                 <h3>Pressure</h3>
-                 <h3>{this.props.weatherData.main.pressure} hPa</h3>
-                 <i className="wi wi-barometer"></i>
-             </div>
+                    <div className="weather-card">
+                        <h3>Pressure</h3>
+                        <h3>{this.props.weatherData.main.pressure} hPa</h3>
+                        <i className="wi wi-barometer"></i>
+                    </div>
 
-             <div className="weather-card">
-                 <h3>Humidity</h3>
-                 <h3>{this.props.weatherData.main.humidity}%</h3>
-                 <i className="wi wi-humidity"></i>
-             </div>
+                    <div className="weather-card">
+                        <h3>Humidity</h3>
+                        <h3>{this.props.weatherData.main.humidity}%</h3>
+                        <i className="wi wi-humidity"></i>
+                    </div>
 
-             <div className="weather-card">
-                 <h3>Wind Speed</h3>
-                 <h3>{this.props.weatherData.wind.speed}mph</h3>
-                 <i className="wi wi-owm-957"></i>
-             </div>
+                    <div className="weather-card">
+                        <h3>Wind Speed</h3>
+                        <h3>{this.props.weatherData.wind.speed}mph</h3>
+                        <i className="wi wi-owm-957"></i>
+                    </div>
 
-             <div className="weather-card">
-                 <h3>Sunrise</h3>
-                 <h3>{this.state.sunrise}</h3>
-                 <i className="wi wi-sunrise"></i>
-             </div>
+                    <div className="weather-card">
+                        <h3>Sunrise</h3>
+                        <h3>{this.state.sunrise}</h3>
+                        <i className="wi wi-sunrise"></i>
+                    </div>
 
-             <div className="weather-card">
-                 <h3>Sunset</h3>
-                 <h3>{this.state.sunset}</h3>
-                 <i className="wi wi-sunset"></i>
-             </div>
+                    <div className="weather-card">
+                        <h3>Sunset</h3>
+                        <h3>{this.state.sunset}</h3>
+                        <i className="wi wi-sunset"></i>
+                    </div>
 
-         </div>
+                </div>
+            </div>
         )
     }
 }
