61a759f9201bbd310219baf34ebc5bd10802aca9
Jack Coldrick <jcldrk@gmail.com>
2017-10-21 15:52:01 +0100

    Refactor CurrentWeatherDetail component to be class based.

--- src/components/CurrentWeatherDetail.js
+++ src/components/CurrentWeatherDetail.js
@@ -1,43 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './css/CurrentWeatherDetail.css';
 
-const CurrentWeatherDetail = props => {
+class CurrentWeatherDetail extends Component {
 
-    const weatherData = props.weatherData;
-
-    if (props.weatherData.weather === undefined) {
-        return null;
+    constructor(props) {
+        super(props);
     }
 
-    return (
-        <div className="current-weather-detail">
+    render() {
+        if (this.props.weatherData.weather === undefined) {
+            return null;
+        }
 
-            <div className="weather-card">
-                <h3>Weather</h3>
-                <h3>{weatherData.weather[0].main}</h3>
-                <i className={`wi wi-${weatherData.weather[0].main.toLowerCase()}`}></i>
-            </div>
+        return (
+            <div className="current-weather-detail">
 
-            <div className="weather-card">
-                <h3>Temperature</h3>
-                <h3>{weatherData.main.temp}&#8451;</h3>
-                <i className="wi wi-thermometer"></i>
-            </div>
+             <div className="weather-card">
+                 <h3>Weather</h3>
+                 <h3>{this.props.weatherData.weather[0].main}</h3>
+                 <i className={`wi wi-${this.props.weatherData.weather[0].main.toLowerCase()}`}></i>
+             </div>
 
-            <div className="weather-card">
-                <h3>Pressure</h3>
-                <h3>{weatherData.main.pressure} hPa</h3>
-                <i className="wi wi-barometer"></i>
-            </div>
+             <div className="weather-card">
+                 <h3>Temperature</h3>
+                 <h3>{this.props.weatherData.main.temp}&#8451;</h3>
+                 <i className="wi wi-thermometer"></i>
+             </div>
 
-            <div className="weather-card">
-                <h3>Humidity</h3>
-                <h3>{weatherData.main.humidity}%</h3>
-                <i className="wi wi-humidity"></i>
-            </div>
+             <div className="weather-card">
+                 <h3>Pressure</h3>
+                 <h3>{this.props.weatherData.main.pressure} hPa</h3>
+                 <i className="wi wi-barometer"></i>
+             </div>
 
-        </div>
-    )
+             <div className="weather-card">
+                 <h3>Humidity</h3>
+                 <h3>{this.props.weatherData.main.humidity}%</h3>
+                 <i className="wi wi-humidity"></i>
+             </div>
+
+         </div>
+        )
+    }
 }
 
 export default CurrentWeatherDetail;
