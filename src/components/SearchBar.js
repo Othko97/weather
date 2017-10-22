779f6df5ba89ef0ebd175f2408190bc99ff5c4f3
Jack Coldrick <jcldrk@gmail.com>
2017-10-22 14:50:15 +0100

    Improved responsivity.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,28 +13,28 @@ class SearchBar extends Component {
         return (
             <div className="search-bar-container">
 
-                <div className="icon-container left">
-                    <i className="fa fa-search" aria-hidden="true"></i>
-                </div>
+                    <div className="icon-container left">
+                        <i className="fa fa-search" aria-hidden="true"></i>
+                    </div>
 
-                <input
-                    type="text"
-                    placeholder="Search for a location."
-                    value={this.state.locationSearchTerm}
-                    onChange={e => this.setState({ locationSearchTerm: e.target.value })}
-                    onKeyPress={this.onKeyPress} />
+                    <input
+                        type="text"
+                        placeholder="Search for a location."
+                        value={this.state.locationSearchTerm}
+                        onChange={e => this.setState({ locationSearchTerm: e.target.value })}
+                        onKeyPress={this.onKeyPress} />
 
-                <div className="icon-container middle" onClick={this.onSearchClick}>
-                    <i
-                        className="fa fa-search"
-                        aria-hidden="true">
-                    </i>
-                </div>
+                    <div className="icon-container middle" onClick={this.onSearchClick}>
+                        <i
+                            className="fa fa-search"
+                            aria-hidden="true">
+                        </i>
+                    </div>
 
-                <div className="icon-container right" onClick={this.onLocationClick}>
-                    <i className="fa fa-map-marker">
-                    </i>
-                </div>
+                    <div className="icon-container right" onClick={this.onLocationClick}>
+                        <i className="fa fa-map-marker">
+                        </i>
+                    </div>
 
             </div>
         )
@@ -47,7 +47,7 @@ class SearchBar extends Component {
         // Resets locationSearchTerm.
         this.setState({ locationSearchTerm: '' });
     }
-    
+
     // Handle click of location button by calling getWeatherByCoords function with current coordinates.
     onLocationClick = () => {
         this.props.getWeatherByCoords(this.props.location.latitude, this.props.location.longitude);
